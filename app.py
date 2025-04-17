602a81ef48a071d8c83e23db82650ae2ddcd9bd4
GuilhermeHobbs <guilhermehobbs@hotmail.com>
2025-04-17 14:44:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ class BigramLanguageModel(nn.Module):
 model = BigramLanguageModel()
 m = model.to(device)
 
-m.load_state_dict(torch.load('data1704.pth', map_location=torch.device('cpu')))
+m.load_state_dict(torch.load('last1704.pth', map_location=torch.device('cpu')))
 
 model.eval()  # Disable dropout
 
